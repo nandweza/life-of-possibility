d1360e1df43c905a0300c0894e5877afcc474b96
nandweza <allannandweza@gmail.com>
2026-03-20 12:09:05 -0600

    added education page layout, data, and scripts to select education path by state

---
+++ Js/educationData.js
@@ -0,0 +1,55 @@
+export const educationData = {
+    idaho: {
+        name: "Idaho",
+
+        communityColleges: [
+            {
+                name: "College of Western Idaho",
+                link: "https://cwi.edu"
+            },
+            {
+                name: "College of Southern Idaho",
+                link: "https://csi.edu"
+            },
+            {
+                name: "North Idaho College",
+                link: "https://nic.edu"
+            },
+        ],
+
+        universities: [
+            {
+                name: "Boise State University",
+                link: "https://www.boisestate.edu"
+            },
+            {
+                name: "University of Idaho",
+                link: "https://www.uidaho.edu"
+            }
+        ],
+
+        ged: {
+            description: "Earn your GED and open doors to education and better jobs.",
+            link: "https://ged.com"
+        },
+
+        resources: [
+            {
+                name: "Idaho Adult Education",
+                link: "#"
+            },
+            {
+                name: "Workforce Training Programs",
+                link: "#"
+            }
+        ]
+    },
+
+    california: {
+        name: "California",
+    },
+
+    oregon: {
+        name: "Oregon",
+    }
+};
--- Js/script.js
+++ Js/script.js
@@ -1,4 +1,4 @@
-"use-strict";
+import { educationData } from "./educationData.js";
 
 function googleTranslateElementInit() {
 	new google.translate.TranslateElement(
@@ -21,13 +21,49 @@ menuToggle.addEventListener("click", () => {
 let timeout;
 const header = document.querySelector(".header");
 
-window.addEventListener("scroll", () => {
-	clearTimeout(timeout);
-	timeout = setTimeout(() => {
-		if(window.scrollY > 80) {
-			header.classList.add("scrolled");
-		} else {
-			header.classList.remove("scrolled");
-		}
-	}, 20); // adjust delay; fixed header shaky
+document.addEventListener("DOMContentLoaded", () => {
+	window.addEventListener("scroll", () => {
+		clearTimeout(timeout);
+		timeout = setTimeout(() => {
+			if(window.scrollY > 80) {
+				header.classList.add("scrolled");
+			} else {
+				header.classList.remove("scrolled");
+			}
+		}, 20); // adjust delay; fixed header shaky
+	});
+});
+
+/**
+ * renderState function updates the education page (The section about education path) 
+ * based on selected state
+ * @param stateKey
+ */
+
+function renderState(stateKey) {
+	const data = educationData[stateKey];
+
+	if (!data) return;
+
+	document.getElementById("educationResults").innerHTML = `
+		<h3>Education in ${data.name}</h3>
+		<div class="grid">
+			<div class="card">
+			<h4><i class="fa-solid fa-building-columns icons"></i>&nbsp;Community Colleges</h4>
+			<ul>
+				${data.communityColleges.map(c => `
+					<li><a href="${c.link}" target="_blank">${c.name}</a></li>
+				`).join("")}
+			</ul>
+		</div>
+		<div class="card">
+			<h4><i class="fa-solid fa-graduation-cap icons"></i>&nbsp;Universities</h4>
+		</div>
+		</div>
+	`;
+}
+
+/* select event listener */
+document.getElementById("stateSelect").addEventListener("change", function () {
+	renderState(this.value);
 });
